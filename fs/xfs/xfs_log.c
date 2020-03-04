414cfb2183d3ae247d7521dd41d5ce0bdb972143
Dave Chinner <dchinner@redhat.com>
2020-03-04 18:07:10 +1100

    xfs: kill XLOG_TIC_INITED
    It is not longer used or checked by anything, so remove the last
    traces from the log ticket code.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/xfs_log.c
+++ fs/xfs/xfs_log.c
@@ -3529,7 +3529,6 @@ xlog_ticket_alloc(
 	tic->t_ocnt		= cnt;
 	tic->t_tid		= prandom_u32();
 	tic->t_clientid		= client;
-	tic->t_flags		= XLOG_TIC_INITED;
 	if (permanent)
 		tic->t_flags |= XLOG_TIC_PERM_RESERV;
 
