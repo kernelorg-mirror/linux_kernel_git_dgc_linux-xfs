49ec7f84d8409802d4d9bacca8a4388639ac17c1
Dave Chinner <dchinner@redhat.com>
2020-03-05 12:34:40 +1100

    xfs: rename btree cursur private btree member flags
    BPRV is not longer appropriate because bc_private is going away.
    Script:
    
    $ sed -i 's/BTCUR_BPRV/BC_BT/g' fs/xfs/*[ch] fs/xfs/*/*[ch]
    
    With manual cleanup to the definitions in fs/xfs/libxfs/xfs_btree.h
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/libxfs/xfs_btree.c
+++ fs/xfs/libxfs/xfs_btree.c
@@ -1743,7 +1743,7 @@ xfs_btree_lookup_get_block(
 
 	/* Check the inode owner since the verifiers don't. */
 	if (xfs_sb_version_hascrc(&cur->bc_mp->m_sb) &&
-	    !(cur->bc_bt.flags & XFS_BTCUR_BPRV_INVALID_OWNER) &&
+	    !(cur->bc_bt.flags & XFS_BC_BT_INVALID_OWNER) &&
 	    (cur->bc_flags & XFS_BTREE_LONG_PTRS) &&
 	    be64_to_cpu((*blkp)->bb_u.l.bb_owner) !=
 			cur->bc_bt.ip->i_ino)
