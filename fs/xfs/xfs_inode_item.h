e695c88d107f0e5a15150571a9fb689da9f9382b
Dave Chinner <dchinner@redhat.com>
2020-05-21 16:50:05 +1000

    xfs: fold xfs_istale_done into xfs_iflush_done
    Having different io completion callbacks for different inode states
    makes things complex. We can detect if the inode is stale via the
    XFS_ISTALE flag in IO completion, so we don't need a special
    callback just for this.
    
    This means inodes only have a single iodone callback, and inode IO
    completion is entirely buffer centric at this point. Hence we no
    longer need to use a log item callback at all as we can just call
    xfs_iflush_done() directly from the buffer completions and walk the
    buffer log item list to complete the all inodes under IO.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/xfs_inode_item.h
+++ fs/xfs/xfs_inode_item.h
@@ -25,15 +25,14 @@ struct xfs_inode_log_item {
 	unsigned int		ili_fsync_fields;  /* logged since last fsync */
 };
 
-static inline int xfs_inode_clean(xfs_inode_t *ip)
+static inline int xfs_inode_clean(struct xfs_inode *ip)
 {
 	return !ip->i_itemp || !(ip->i_itemp->ili_fields & XFS_ILOG_ALL);
 }
 
 extern void xfs_inode_item_init(struct xfs_inode *, struct xfs_mount *);
 extern void xfs_inode_item_destroy(struct xfs_inode *);
-extern void xfs_iflush_done(struct xfs_buf *, struct xfs_log_item *);
-extern void xfs_istale_done(struct xfs_buf *, struct xfs_log_item *);
+extern void xfs_iflush_done(struct xfs_buf *);
 extern void xfs_iflush_abort(struct xfs_inode *);
 extern int xfs_inode_item_format_convert(xfs_log_iovec_t *,
 					 struct xfs_inode_log_format *);
