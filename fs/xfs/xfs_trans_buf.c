a088a17f2ef42aced6d881c70d1fa28256861e4e
Dave Chinner <dchinner@redhat.com>
2020-05-21 16:45:37 +1000

    xfs: call xfs_buf_iodone directly
    All unmarked dirty buffers should be in the AIL and have log items
    attached to them. Hence when they are written, we will run a
    callback to remove the item from the AIL if appropriate. Now that
    we've handled inode and dquot buffers, all remaining calls are to
    xfs_buf_iodone() and so we can hard code this rather than use an
    indirect call.
    
    Signed-off-by: Dave Chinner <dchinner@redhat.com>

--- fs/xfs/xfs_trans_buf.c
+++ fs/xfs/xfs_trans_buf.c
@@ -465,23 +465,16 @@ xfs_trans_dirty_buf(
 
 	ASSERT(bp->b_transp == tp);
 	ASSERT(bip != NULL);
-	ASSERT(bp->b_iodone == NULL ||
-	       bp->b_iodone == xfs_buf_iodone_callbacks);
 
 	/*
 	 * Mark the buffer as needing to be written out eventually,
 	 * and set its iodone function to remove the buffer's buf log
 	 * item from the AIL and free it when the buffer is flushed
-	 * to disk.  See xfs_buf_attach_iodone() for more details
-	 * on li_cb and xfs_buf_iodone_callbacks().
-	 * If we end up aborting this transaction, we trap this buffer
-	 * inside the b_bdstrat callback so that this won't get written to
-	 * disk.
+	 * to disk.
 	 */
 	bp->b_flags |= XBF_DONE;
 
 	ASSERT(atomic_read(&bip->bli_refcount) > 0);
-	bp->b_iodone = xfs_buf_iodone_callbacks;
 	bip->bli_item.li_cb = xfs_buf_iodone;
 
 	/*
